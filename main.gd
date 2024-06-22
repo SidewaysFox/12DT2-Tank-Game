b554c94c7ca3948d3f3ecea317e3fb1bc2156f4f
Ben <benwhaley.nz@gmail.com>
2024-06-22 16:00:54 +1200

    New map + camera control
    Also finished map name stuff

--- main.gd
+++ main.gd
@@ -9,26 +9,23 @@ var switch_start = true
 var show_map_name = false
 
 var maps = [
-	"res://map_1.tscn",
-	"res://map_2.tscn",
-	"res://map_3.tscn",
-]
-
-var map_names = [
-	"Grasswall",
-	"Grassierwall",
-	"Grassiestwall"
+	["res://map_1.tscn", "Grasswall", Vector2(1, 1)],
+	["res://map_2.tscn", "Grassierwall", Vector2(1, 1)],
+	["res://map_3.tscn", "Grassiestwall", Vector2(1, 1)],
+	["res://map_4.tscn", "Grasswall Duel", Vector2(2, 2)],
 ]
 
 var p1_spawns = [
 	Vector2(300, 540),
 	Vector2(150, 540),
 	Vector2(200, 540),
+	Vector2(580, 540),
 ]
 var p2_spawns = [
 	Vector2(1620, 540),
 	Vector2(1770, 540),
 	Vector2(1720, 540),
+	Vector2(1340, 540),
 ]
 
 
@@ -37,6 +34,7 @@ func _ready():
 	new_map.global_position.x = 0
 	next_map += 1
 	current_map = new_map
+	show_map_name = false
 
 
 func _map_switch(delta, initial: bool):
@@ -45,7 +43,7 @@ func _map_switch(delta, initial: bool):
 		if next_map == len(maps):
 			next_map = 0
 		# Create new map and position it in hierarchy
-		new_map = load(maps[next_map]).instantiate()
+		new_map = load(maps[next_map][0]).instantiate()
 		show_map_name = true
 		new_map.global_position.x = 1920
 		add_child(new_map)
@@ -54,13 +52,15 @@ func _map_switch(delta, initial: bool):
 	
 	# Map transition
 	if not initial:
-		if new_map.global_position.x > 0:
-			new_map.global_position.x -= 3000 * delta
-			current_map.global_position.x -= 3000 * delta
-	
+		if new_map.global_position.x > 1:
+			new_map.global_position = lerp(new_map.global_position, Vector2(0, 0), 0.1)
+			current_map.global_position = lerp(current_map.global_position, Vector2(-1920, 0), 0.1)
+			$Camera2D.zoom = lerp($Camera2D.zoom, maps[next_map][2], 0.1)
+		
 		# End of transition
-		elif new_map.global_position.x <= 0:
+		elif new_map.global_position.x <= 1:
 			new_map.global_position.x = 0
+			$Camera2D.zoom = maps[next_map][2]
 			# Delete previous map and set the new current map
 			show_map_name = false
 			next_map += 1
@@ -78,7 +78,7 @@ func _process(delta):
 		_map_switch(delta, false)
 	
 	if show_map_name:
-		$CanvasLayer/ColorRect.show()
-		$CanvasLayer/ColorRect/MapName.text = map_names[next_map]
+		$CanvasLayer/MapRect.show()
+		$CanvasLayer/MapRect/MapName.text = maps[next_map][1]
 	else:
-		$CanvasLayer/ColorRect.hide()
+		$CanvasLayer/MapRect.hide()
