db7e5a048c36d120d9f8e7568c527e5632117890
Ben <benwhaley.nz@gmail.com>
2024-08-05 16:17:56 +1200

    added pause menu buttons
    not yet functional

--- PauseItems.gd
+++ PauseItems.gd
@@ -3,12 +3,15 @@ extends Control
 
 func _ready():
 	$Paused.hide()
+	$Blur.hide()
 
 
 func _on_pause_button_pressed():
 	if get_tree().paused:
 		get_tree().paused = false
 		$Paused.hide()
+		$Blur.hide()
 	else:
 		get_tree().paused = true
 		$Paused.show()
+		$Blur.show()
