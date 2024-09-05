d1bfe299886d2a12ae76cca87664a78212f1d4af
Ben <benwhaley.nz@gmail.com>
2024-09-05 17:27:38 +1200

    particle adjustments
    they are good now

--- parpicles.gd
+++ parpicles.gd
@@ -1,9 +1,17 @@
 extends GPUParticles2D
 
 
+@onready var main = get_node("/root/Main/")
+@onready var scores = get_node("/root/Main/Scores/")
+
+
 func _ready() -> void:
 	print("emitted")
-	
+
+
+func _process(delta: float) -> void:
+	if main.switching and not scores.active:
+		_on_finished()
 
 
 func _on_finished() -> void:
